35543dee0ecf50fad8b4c228493788c346943adf
Yari Herrera <yariherrera@Yaris-MacBook-Pro.local>
2021-09-30 00:41:17 +0200

    init branch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">Form</router-link>
+    <base-navbar>
+      <span slot="home">
+        <router-link to="/">Home</router-link>
+      </span>
+
+      <span slot="form">
+        <router-link to="/form">Form</router-link>
+      </span>
+    </base-navbar>
+
     <router-view />
   </div>
 </template>
+
+<style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+html {
+  font-size: 62.5%;
+  /* 1rem = 10px */
+  box-sizing: border-box;
+  color: #f1f2f6;
+  line-height: 1.6;
+}
+
+body {
+  font-family: "Raleway", sans-serif !important;
+}
+</style>
---
+++ src/components/basenavbar/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <slot name="home"></slot>
+    <slot name="form"></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" >
+</style>
