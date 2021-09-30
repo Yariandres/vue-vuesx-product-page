3f7817fb05da95a52bcc9700e0a07016fdfb0a84
Yari Herrera <yari.andres@gmail.com>
2021-10-01 00:16:30 +0200

    adding todo notes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <b-container>
     <div class="my-5">
+      <!-- TODO 
+        listen to the event and update carsDisplay var
+       -->
       <base-toggle-view-component></base-toggle-view-component>
     </div>
 
@@ -10,14 +13,11 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   data() {
     return {
       cardsDisplay: "",
     };
   },
-
-  methods: {},
 };
 </script>
